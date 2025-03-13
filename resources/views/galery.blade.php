279cb2a04bd552363d77febd806f0b7f4cfd5a0d
Ari Aliansyah <ari.aliansyah@fuse.co.id>
2025-03-13 10:36:00 +0700

    update

--- resources/views/galery.blade.php
+++ resources/views/galery.blade.php
@@ -5,8 +5,9 @@
     <h2 class="text-3xl font-bold text-center text-pink-600">📸 Kenangan Indah Kita</h2>
     <div class="grid grid-cols-2 gap-4 mt-6 md:grid-cols-3">
         @foreach(['1', '2', '3', '4', '5', '6'] as $img)
-            <img src="https://source.unsplash.com/400x300/?love,{{ $img }}" class="rounded-lg shadow-md">
+        <img src="https://source.unsplash.com/400x300/?love,{{ $img }}" class="rounded-lg shadow-md">
         @endforeach
     </div>
 </div>
 @endsection
+{{-- test --}}
